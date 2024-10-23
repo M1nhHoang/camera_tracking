181cf5d686aed21b4586b1493a6f2091846aed29
minhhoang <boydammedamsamcotdien@gmail.com>
2024-10-23 22:03:12 +0700

    update docker file, add redis database

--- database_service/main.py
+++ database_service/main.py
@@ -11,4 +11,4 @@ app.include_router(detected.router, prefix="/detected", tags=["detected"])
 
 # run server
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8003, workers=1)
+    uvicorn.run("main:app", host="0.0.0.0", port=8003, workers=4)
--- face_embedding_service/main.py
+++ face_embedding_service/main.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append(".")
 
 import numpy as np
+import uvicorn
 
 from PIL import Image
 from io import BytesIO
@@ -21,3 +22,8 @@ async def get_embedding(file: UploadFile = File(...)):
     embedding = DeepFace.represent(image, model_name="VGG-Face")
 
     return {"embedding": embedding[0]["embedding"]}
+
+
+# run server
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, workers=2)
